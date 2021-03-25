27d20963b683bb3878c64acf1277382bb63dc385
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-03-24 22:08:55 -0400

    Updated model package w/ LoadDatabase.java class

---
+++ src/main/java/com/springboot/rest/api/payroll/model/LoadDatabase.java
@@ -0,0 +1,29 @@
+package com.springboot.rest.api.payroll.model;
+
+import com.springboot.rest.api.payroll.repository.EmployeeRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/* Spring Boot will run ALL CommandLineRunner beans once the application context is loaded.
+ * This runner will request a copy of the EmployeeRepository you just created.
+ * Using it, it will create two entities and store them.*/
+
+@Configuration
+class LoadDatabase {
+
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+  @Bean
+  CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+    return args -> {
+      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+    };
+  }
+}
