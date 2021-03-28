3aa0f6518db83b4a4a604f6b33fae2487c9d23b6
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-03-27 22:32:07 -0400

    updated the EmployeeController class

--- src/main/java/com/springboot/rest/api/payroll/controller/EmployeeController.java
+++ src/main/java/com/springboot/rest/api/payroll/controller/EmployeeController.java
@@ -131,6 +131,30 @@ public
 
     return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
+  
+  //Handling a PUT for different clients; Replace Employee method
+  @PutMapping("/employees/{id}")
+  ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+    Employee updatedEmployee = repository.findById(id) //
+        .map(employee -> {
+          employee.setName(newEmployee.getName());
+          employee.setRole(newEmployee.getRole());
+          return repository.save(employee);
+        }) //
+        .orElseGet(() -> {
+          newEmployee.setId(id);
+          return repository.save(newEmployee);
+        });
+
+    EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+    return ResponseEntity //
+        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+        .body(entityModel);
+  }
+  
+  /* Old Replace Employee method
   @PutMapping("/employees/{id}")
   Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
     
@@ -144,7 +168,7 @@ public
         newEmployee.setId(id);
         return repository.save(newEmployee);
       });
-  }
+  } */
   
   
  @GetMapping("/employees")
