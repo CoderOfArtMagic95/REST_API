2f2f892318c388e58503cfc9cb904c015b430a85
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-03-28 20:43:41 -0400

    Updated LoadDatabase and OrderController

--- src/main/java/com/springboot/rest/api/payroll/controller/OrderController.java
+++ src/main/java/com/springboot/rest/api/payroll/controller/OrderController.java
@@ -1,19 +1,25 @@
 package com.springboot.rest.api.payroll.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.rest.api.payroll.model.Order;
 import com.springboot.rest.api.payroll.model.OrderModelAssembler;
 import com.springboot.rest.api.payroll.model.Status;
-
 import com.springboot.rest.api.payroll.repository.OrderRepository;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -63,5 +69,48 @@ public
     return ResponseEntity //
         .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri()) //
         .body(assembler.toModel(newOrder));
+  }
+  //cancels the order
+  /* It checks the Order status before allowing it to be cancelled.
+   * If it’s not a valid state, it returns an RFC-7807 Problem, a 
+   * hypermedia-supporting error container. If the transition is indeed
+   * valid, it transitions the Order to CANCELLED.*/
+  @DeleteMapping("/orders/{id}/cancel")
+  ResponseEntity<?> cancel(@PathVariable Long id) {
+
+    Order order = orderRepository.findById(id) //
+        .orElseThrow(() -> new OrderNotFoundException(id));
+
+    if (order.getStatus() == Status.IN_PROGRESS) {
+      order.setStatus(Status.CANCELLED);
+      return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+    }
+
+    return ResponseEntity //
+        .status(HttpStatus.METHOD_NOT_ALLOWED) //
+        .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+        .body(Problem.create() //
+            .withTitle("Method not allowed") //
+            .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+  }
+  
+  //completes the order
+  @PutMapping("/orders/{id}/complete")
+  ResponseEntity<?> complete(@PathVariable Long id) {
+
+    Order order = orderRepository.findById(id) //
+        .orElseThrow(() -> new OrderNotFoundException(id));
+
+    if (order.getStatus() == Status.IN_PROGRESS) {
+      order.setStatus(Status.COMPLETED);
+      return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+    }
+
+    return ResponseEntity //
+        .status(HttpStatus.METHOD_NOT_ALLOWED) //
+        .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+        .body(Problem.create() //
+            .withTitle("Method not allowed") //
+            .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
   }
 }
--- src/main/java/com/springboot/rest/api/payroll/model/LoadDatabase.java
+++ src/main/java/com/springboot/rest/api/payroll/model/LoadDatabase.java
@@ -1,6 +1,7 @@
 package com.springboot.rest.api.payroll.model;
 
 import com.springboot.rest.api.payroll.repository.EmployeeRepository;
+import com.springboot.rest.api.payroll.repository.OrderRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,18 +13,28 @@ import org.springframework.context.annotation.Configuration;
 /* Spring Boot will run ALL CommandLineRunner beans once the application context is loaded.
  * This runner will request a copy of the EmployeeRepository you just created.
  * Using it, it will create two entities and store them.*/
-
 @Configuration
 class LoadDatabase {
 
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar","Bilbo Baggins")));
-      log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief", "Frodo Baggins")));
+      employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar", "Bilbo Baggins"));
+      employeeRepository.save(new Employee("Frodo", "Baggins", "thief", "Frodo Baggins"));
+
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+      
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      });
+      
     };
   }
 }
